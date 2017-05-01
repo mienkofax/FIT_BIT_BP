7daa23815327c4f4d93620d98bcc477c5066098a
Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
2017-05-01 18:35:08 +0200

    DeviceManager: new clas
    Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>

---
+++ src/core/DeviceManager.cpp
@@ -0,0 +1,50 @@
+#include "core/DeviceManager.h"
+#include "util/ZMQUtil.h"
+
+using namespace BeeeOn;
+
+DeviceManager::DeviceManager():
+	m_stop(false),
+	m_zmqClient(new ZMQClient()),
+	m_prefix(DevicePrefix::fromRaw(DevicePrefix::PREFIX_INVALID))
+{
+	m_runner.addRunnable(m_zmqClient);
+}
+
+void DeviceManager::stop()
+{
+	m_runner.stop();
+	m_stop = true;
+}
+
+void DeviceManager::setPrefixName(const std::string &name)
+{
+	m_prefix = DevicePrefix::parse(name);
+	m_zmqClient->setDeviceManagerPrefix(m_prefix);
+}
+
+void DeviceManager::setDataServerHost(const std::string &host)
+{
+	m_zmqClient->setDataServerHost(host);
+}
+
+void DeviceManager::setHelloServerHost(const std::string &host)
+{
+	m_zmqClient->setHelloServerHost(host);
+}
+
+void DeviceManager::setDataServerPort(const int port)
+{
+	m_zmqClient->setDataServerPort(port);
+}
+
+void DeviceManager::setHelloServerPort(const int port)
+{
+	m_zmqClient->setHelloServerPort(port);
+}
+
+void DeviceManager::runClient()
+{
+	m_runner.start();
+}
+
---
+++ src/core/DeviceManager.h
@@ -0,0 +1,58 @@
+#ifndef BEEEON_DEVICE_MANAGER_H
+#define BEEEON_DEVICE_MANAGER_H
+
+#include <Poco/AtomicCounter.h>
+#include <Poco/SharedPtr.h>
+
+#include "core/Answer.h"
+#include "core/Command.h"
+#include "loop/StoppableRunnable.h"
+#include "loop/LoopRunner.h"
+#include "model/GlobalID.h"
+#include "util/Loggable.h"
+#include "zmq/ZMQClient.h"
+
+namespace BeeeOn {
+
+class DeviceManager : public Loggable, public StoppableRunnable {
+public:
+	DeviceManager();
+
+	void stop();
+
+	void setDataServerHost(const std::string &host);
+	void setHelloServerHost(const std::string &host);
+	void setDataServerPort(const int port);
+	void setHelloServerPort(const int port);
+	void setPrefixName(const std::string &prefixName);
+
+protected:
+	/*
+	 * Struktura reprezentujuca potrebne udaje ktore sa ulozia do mapy
+	 * m_table spolu s answer a su potrebne pre prijatie spravne
+	 * priradenie prijatej spravy.
+	 */
+	struct ResultData {
+		GlobalID id;
+		Command::Ptr cmd;
+	};
+
+	/*
+	 * Rozhranie pre prijem dat.
+	 */
+	virtual void onEvent(const void*, ZMQMessage &zmqMessage) = 0;
+
+	void runClient();
+
+protected:
+	Poco::AtomicCounter m_stop;
+	DevicePrefix m_prefix;
+	LoopRunner m_runner;
+	std::map<Answer::Ptr, ResultData> m_table;
+	Poco::SharedPtr<ZMQClient> m_zmqClient;
+};
+
+}
+
+
+#endif
