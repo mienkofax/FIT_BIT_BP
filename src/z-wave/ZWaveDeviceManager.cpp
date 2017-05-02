67f0999d35dc412c152a76b7f6afb56773c12c2a
Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>
2017-05-02 03:38:31 +0200

    ZMQDeviceManager: init
    Signed-off-by: Peter Tisovčík <xtisov00@stud.fit.vutbr.cz>

---
+++ src/z-wave/ZWaveDeviceManager.cpp
@@ -0,0 +1,86 @@
+#include <Options.h>
+#include <Manager.h>
+
+#include "di/Injectable.h"
+#include "z-wave/ZWaveDeviceManager.h"
+
+BEEEON_OBJECT_BEGIN(BeeeOn, ZWaveDeviceManager)
+BEEEON_OBJECT_CASTABLE(StoppableRunnable)
+BEEEON_OBJECT_TEXT("dataServerHost", &ZWaveDeviceManager::setDataServerHost)
+BEEEON_OBJECT_NUMBER("dataServerPort", &ZWaveDeviceManager::setDataServerPort)
+BEEEON_OBJECT_TEXT("helloServerHost", &ZWaveDeviceManager::setHelloServerHost)
+BEEEON_OBJECT_NUMBER("helloServerPort", &ZWaveDeviceManager::setHelloServerPort)
+BEEEON_OBJECT_TEXT("prefixName", &ZWaveDeviceManager::setPrefixName)
+BEEEON_OBJECT_TEXT("setUserPath", &ZWaveDeviceManager::setUserPath)
+BEEEON_OBJECT_TEXT("donglePath", &ZWaveDeviceManager::setDonglePath)
+BEEEON_OBJECT_TEXT("setConfigPath", &ZWaveDeviceManager::setConfigPath)
+BEEEON_OBJECT_NUMBER("setPollInterval", &ZWaveDeviceManager::setPollInterval)
+BEEEON_OBJECT_NUMBER("setDriverMaxAttempts", &ZWaveDeviceManager::setDriverMaxAttempts)
+BEEEON_OBJECT_NUMBER("setSaveConfigurationFile", &ZWaveDeviceManager::setSaveConfigurationFile)
+
+BEEEON_OBJECT_END(BeeeOn, ZWaveDeviceManager)
+
+using namespace BeeeOn;
+using namespace OpenZWave;
+
+void ZWaveDeviceManager::onEvent(const void *, ZMQMessage &zmqMessage)
+{
+
+}
+
+void ZWaveDeviceManager::setUserPath(const std::string &userPath)
+{
+	m_userPath = userPath;
+}
+
+void ZWaveDeviceManager::setDonglePath(const std::string &donglePath)
+{
+	m_donglePath = donglePath;
+}
+
+void ZWaveDeviceManager::setConfigPath(const std::string &configPath)
+{
+	m_configPath = configPath;
+}
+
+void ZWaveDeviceManager::setPollInterval(int pollInterval)
+{
+	m_pollInterval = pollInterval;
+}
+
+void ZWaveDeviceManager::setDriverMaxAttempts(int maxAttempts)
+{
+	m_driverMaxAttempts = maxAttempts;
+}
+
+void ZWaveDeviceManager::setSaveConfigurationFile(bool save)
+{
+	m_saveConfigurationFile = save;
+}
+
+void ZWaveDeviceManager::installOption()
+{
+	OpenZWave::Options::Create(m_configPath, m_userPath, "");
+	Options::Get()->AddOptionInt("PollInterval", m_pollInterval);
+	Options::Get()->AddOptionBool("logging", true);
+	Options::Get()->AddOptionBool("SaveConfiguration", m_saveConfigurationFile);
+	Options::Get()->AddOptionInt("DriverMaxAttempts", m_driverMaxAttempts);
+	Options::Get()->Lock();
+}
+
+void ZWaveDeviceManager::run()
+{
+	installOption();
+
+	DeviceManager::runClient();
+
+	Manager::Create();
+}
+
+void ZWaveDeviceManager::stop()
+{
+	Manager::Destroy();
+	Options::Destroy();
+
+	DeviceManager::stop();
+}
---
+++ src/z-wave/ZWaveDeviceManager.h
@@ -0,0 +1,38 @@
+#ifndef BEEEON_ZWAVE_DEVICE_MANAGER_H
+#define BEEEON_ZWAVE_DEVICE_MANAGER_H
+
+#include <string.h>
+
+#include "core/DeviceManager.h"
+
+namespace BeeeOn {
+
+class ZWaveDeviceManager : public DeviceManager {
+public:
+	void run() override;
+	void stop() override;
+
+	void setUserPath(const std::string &userPath);
+	void setDonglePath(const std::string &donglePath);
+	void setConfigPath(const std::string &configPath);
+	void setPollInterval(int pollInterval);
+	void setDriverMaxAttempts(int maxAttempts);
+	void setSaveConfigurationFile(bool save);
+
+protected:
+	void onEvent(const void*, ZMQMessage &zmqMessage) override;
+
+	void installOption();
+
+protected:
+	std::string m_userPath;
+	std::string m_donglePath;
+	std::string m_configPath;
+	int m_pollInterval;
+	int m_driverMaxAttempts;
+	bool m_saveConfigurationFile;
+};
+
+}
+
+#endif
